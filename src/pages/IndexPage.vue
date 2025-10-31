85621a3257fb1a39e031fcb9a8254c08f185c06d
MEYLYMENDOZA <22200248@ue.edu.pe>
2025-10-30 20:21:01 -0500

    Primer intentoo

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,25 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+  <q-page padding class="flex flex-center">
+    <q-card flat bordered style="width: 350px;">
+      <q-card-section class="text-center">
+        <q-avatar size="100px">
+          <img src="https://cdn.quasar.dev/img/avatar.png">
+        </q-avatar>
+        <h3 class="q-mt-md">Meyly Mendoza</h3>
+        <p>Estudiante de Desarrollo de Software</p>
+      </q-card-section>
+
+      <q-card-section>
+        Soy una persona responsable, dedicada y con interés en continuar aprendiendo 
+        tecnologías modernas para el desarrollo de aplicaciones web.
+      </q-card-section>
+
+      <q-card-actions align="center">
+        <q-btn color="primary" label="Ver Portafolio" />
+      </q-card-actions>
+    </q-card>
   </q-page>
 </template>
 
 <script setup>
-//
 </script>
